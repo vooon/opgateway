8df4b7e01a27faff87f4b9914c17e6a2fcd0a8f5
Vladimir Ermakov <vooon341@gmail.com>
2013-07-03 13:59:17 +0400

    UAVO: add UAVObject class (based on Qt version)

---
+++ src/uavobjects/uavobject.cpp
@@ -0,0 +1,317 @@
+/**
+ ******************************************************************************
+ * @file       uavobject.cpp
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @author     Vladimir Ermakov, Copyright (C) 2013.
+ * @see        The GNU Public License (GPL) Version 3
+ * @brief      The UAVUObjects
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "uavobject.h"
+
+// Macros
+#define SET_BITS(var, shift, value, mask) var = (var & ~(mask << shift)) | (value << shift);
+
+#define UAVOBJ_FLT_ACCESS_SHIFT				0
+#define UAVOBJ_GCS_ACCESS_SHIFT				1
+#define UAVOBJ_FLT_TELEMETRY_ACKED_SHIFT		2
+#define UAVOBJ_GCS_TELEMETRY_ACKED_SHIFT		3
+#define UAVOBJ_FLT_TELEMETRY_UPDATE_MODE_SHIFT		4
+#define UAVOBJ_GCS_TELEMETRY_UPDATE_MODE_SHIFT		6
+#define UAVOBJ_UPDATE_MODE_MASK				0x3
+
+using namespace openpilot;
+
+/** Constructor
+ * @param objID The object ID
+ * @param isSingleInst True if this object can only have a single instance
+ * @param name Object name
+ */
+UAVObject::UAVObject(uint32_t objID, bool isSingleInst, const std::string &name)
+{
+	this->objID  = objID;
+	this->instID = 0;
+	this->isSingleInst = isSingleInst;
+	this->name   = name;
+}
+
+/** Initialize object with its instance ID
+ */
+void UAVObject::initialize(uint32_t instID)
+{
+	boost::recursive_timed_mutex::scoped_lock(mutex);
+
+	this->instID = instID;
+}
+
+/** Get the object ID
+ */
+uint32_t UAVObject::getObjID()
+{
+	return objID;
+}
+
+/** Get the instance ID
+ */
+uint32_t UAVObject::getInstID()
+{
+	return instID;
+}
+
+/** Returns true if this is a single instance object
+*/
+bool UAVObject::isSingleInstance()
+{
+	return isSingleInst;
+}
+
+/** Get the name of the object
+*/
+std::string UAVObject::getName()
+{
+	return name;
+}
+
+/** Get the total number of bytes of the object's data
+*/
+size_t UAVObject::getNumBytes()
+{
+	return numBytes;
+}
+
+/** Request that this object is updated with the latest values from the autopilot
+*/
+void UAVObject::requestUpdate()
+{
+	updateRequested(this);
+}
+
+/** Signal that the object has been updated
+*/
+void UAVObject::updated()
+{
+	objectUpdatedManual(this);
+	objectUpdated(this);
+}
+
+/** Lock mutex of this object
+*/
+void UAVObject::lock()
+{
+	mutex.lock();
+}
+
+/** Lock mutex of this object
+*/
+bool UAVObject::lock(int timeoutMs)
+{
+	boost::system_time t = boost::get_system_time() +
+		boost::posix_time::milliseconds(timeoutMs);
+
+	return mutex.timed_lock(t);
+}
+
+/** Unlock mutex of this object
+*/
+void UAVObject::unlock()
+{
+	mutex.unlock();
+}
+
+/** Get object's mutex
+*/
+boost::recursive_timed_mutex *UAVObject::getMutex()
+{
+	return &mutex;
+}
+
+/** Return a string with the object information (only the header)
+ */
+std:string UAVObject::toStringBrief()
+{
+	std::string sout << getName() <<
+		" (ID: " << getObjID() <<
+		", InstID: " << getInstID() <<
+		"NumBytes: " << getNumBytes() <<
+		"SInst: " << isSingleInstance() <<
+		")" << std::endl;
+	return sout;
+}
+
+#if 0
+/** Pack the object data into a byte array
+ * @returns The number of bytes copied
+ */
+ssize_t UAVObject::serialize(uint8_t *dataOut)
+{
+	QMutexLocker locker(mutex);
+	qint32 offset = 0;
+
+	for (int n = 0; n < fields.length(); ++n) {
+	    fields[n]->pack(&dataOut[offset]);
+	    offset += fields[n]->getNumBytes();
+	}
+
+	return numBytes;
+}
+
+/** Unpack the object data from a byte array
+ * @returns The number of bytes copied
+ */
+ssize_t UAVObject::desirialize(const uint8_t *dataIn)
+{
+	QMutexLocker locker(mutex);
+	qint32 offset = 0;
+
+	for (int n = 0; n < fields.length(); ++n) {
+	    fields[n]->unpack(&dataIn[offset]);
+	    offset += fields[n]->getNumBytes();
+	}
+
+	objectUnpacked(this); // trigger object updated event
+	objectUpdated(this);
+
+	return numBytes;
+}
+#endif
+
+/** Initialize a UAVObjMetadata object.
+ * \param[in] metadata The metadata object
+ */
+void UAVObject::MetadataInitialize(UAVObject::Metadata &metadata)
+{
+    metadata.flags =
+        ACCESS_READWRITE << UAVOBJ_FLT_ACCESS_SHIFT |
+        ACCESS_READWRITE << UAVOBJ_GCS_ACCESS_SHIFT |
+        1 << UAVOBJ_FLT_TELEMETRY_ACKED_SHIFT |
+        1 << UAVOBJ_GCS_TELEMETRY_ACKED_SHIFT |
+        UPDATEMODE_ONCHANGE << UAVOBJ_FLT_TELEMETRY_UPDATE_MODE_SHIFT |
+        UPDATEMODE_ONCHANGE << UAVOBJ_GCS_TELEMETRY_UPDATE_MODE_SHIFT;
+    metadata.flightTelemetryUpdatePeriod = 0;
+    metadata.gcsTelemetryUpdatePeriod    = 0;
+    metadata.loggingUpdatePeriod = 0;
+}
+
+/** Get the UAVObject metadata access member
+ * \param[in] metadata The metadata object
+ * \return the access type
+ */
+UAVObject::AccessMode UAVObject::GetFlightAccess(const UAVObject::Metadata &metadata)
+{
+    return UAVObject::AccessMode((metadata.flags >> UAVOBJ_FLT_ACCESS_SHIFT) & 1);
+}
+
+/** Set the UAVObject metadata access member
+ * \param[in] metadata The metadata object
+ * \param[in] mode The access mode
+ */
+void UAVObject::SetFlightAccess(UAVObject::Metadata &metadata, UAVObject::AccessMode mode)
+{
+    SET_BITS(metadata.flags, UAVOBJ_FLT_ACCESS_SHIFT, mode, 1);
+}
+
+/** Get the UAVObject metadata GCS access member
+ * \param[in] metadata The metadata object
+ * \return the GCS access type
+ */
+UAVObject::AccessMode UAVObject::GetGcsAccess(const UAVObject::Metadata &metadata)
+{
+    return UAVObject::AccessMode((metadata.flags >> UAVOBJ_GCS_ACCESS_SHIFT) & 1);
+}
+
+/** Set the UAVObject metadata GCS access member
+ * \param[in] metadata The metadata object
+ * \param[in] mode The access mode
+ */
+void UAVObject::SetGcsAccess(UAVObject::Metadata &metadata, UAVObject::AccessMode mode)
+{
+    SET_BITS(metadata.flags, UAVOBJ_GCS_ACCESS_SHIFT, mode, 1);
+}
+
+/** Get the UAVObject metadata telemetry acked member
+ * \param[in] metadata The metadata object
+ * \return the telemetry acked boolean
+ */
+bool UAVObject::GetFlightTelemetryAcked(const UAVObject::Metadata &metadata)
+{
+    return (metadata.flags >> UAVOBJ_FLT_TELEMETRY_ACKED_SHIFT) & 1;
+}
+
+/** Set the UAVObject metadata telemetry acked member
+ * \param[in] metadata The metadata object
+ * \param[in] val The telemetry acked boolean
+ */
+void UAVObject::SetFlightTelemetryAcked(UAVObject::Metadata &metadata, bool val)
+{
+    SET_BITS(metadata.flags, UAVOBJ_FLT_TELEMETRY_ACKED_SHIFT, val, 1);
+}
+
+/** Get the UAVObject metadata GCS telemetry acked member
+ * \param[in] metadata The metadata object
+ * \return the telemetry acked boolean
+ */
+bool UAVObject::GetGcsTelemetryAcked(const UAVObject::Metadata &metadata)
+{
+    return (metadata.flags >> UAVOBJ_GCS_TELEMETRY_ACKED_SHIFT) & 1;
+}
+
+/** Set the UAVObject metadata GCS telemetry acked member
+ * \param[in] metadata The metadata object
+ * \param[in] val The GCS telemetry acked boolean
+ */
+void UAVObject::SetGcsTelemetryAcked(UAVObject::Metadata &metadata, bool val)
+{
+    SET_BITS(metadata.flags, UAVOBJ_GCS_TELEMETRY_ACKED_SHIFT, val, 1);
+}
+
+/** Get the UAVObject metadata telemetry update mode
+ * \param[in] metadata The metadata object
+ * \return the telemetry update mode
+ */
+UAVObject::UpdateMode UAVObject::GetFlightTelemetryUpdateMode(const UAVObject::Metadata &metadata)
+{
+    return UAVObject::UpdateMode((metadata.flags >> UAVOBJ_FLT_TELEMETRY_UPDATE_MODE_SHIFT) & UAVOBJ_UPDATE_MODE_MASK);
+}
+
+/** Set the UAVObject metadata telemetry update mode member
+ * \param[in] metadata The metadata object
+ * \param[in] val The telemetry update mode
+ */
+void UAVObject::SetFlightTelemetryUpdateMode(UAVObject::Metadata &metadata, UAVObject::UpdateMode val)
+{
+    SET_BITS(metadata.flags, UAVOBJ_FLT_TELEMETRY_UPDATE_MODE_SHIFT, val, UAVOBJ_UPDATE_MODE_MASK);
+}
+
+/** Get the UAVObject metadata GCS telemetry update mode
+ * \param[in] metadata The metadata object
+ * \return the GCS telemetry update mode
+ */
+UAVObject::UpdateMode UAVObject::GetGcsTelemetryUpdateMode(const UAVObject::Metadata &metadata)
+{
+    return UAVObject::UpdateMode((metadata.flags >> UAVOBJ_GCS_TELEMETRY_UPDATE_MODE_SHIFT) & UAVOBJ_UPDATE_MODE_MASK);
+}
+
+/** Set the UAVObject metadata GCS telemetry update mode member
+ * \param[in] metadata The metadata object
+ * \param[in] val The GCS telemetry update mode
+ */
+void UAVObject::SetGcsTelemetryUpdateMode(UAVObject::Metadata &metadata, UAVObject::UpdateMode val)
+{
+    SET_BITS(metadata.flags, UAVOBJ_GCS_TELEMETRY_UPDATE_MODE_SHIFT, val, UAVOBJ_UPDATE_MODE_MASK);
+}
+
---
+++ src/uavobjects/uavobject.h
@@ -0,0 +1,139 @@
+/**
+ ******************************************************************************
+ * @file       uavobject.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @author     Vladimir Ermakov, Copyright (C) 2013.
+ * @see        The GNU Public License (GPL) Version 3
+ * @brief      The UAVUObjects
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef UAVOBJECT_H
+#define UAVOBJECT_H
+
+#include <string>
+#include <boost/signals2.hpp>
+#include <boost/thread/recursive_mutex.hpp>
+
+namespace openpilot
+{
+
+class UAVObject {
+public:
+
+	/** Object update mode
+	 */
+	typedef enum {
+		UPDATEMODE_MANUAL    = 0,	/** Manually update object, by calling the updated() function */
+		UPDATEMODE_PERIODIC  = 1,	/** Automatically update object at periodic intervals */
+		UPDATEMODE_ONCHANGE  = 2,	/** Only update object when its data changes */
+		UPDATEMODE_THROTTLED = 3	/** Object is updated on change, but not more often than the interval time */
+	} UpdateMode;
+
+	/** Access mode
+	 */
+	typedef enum {
+		ACCESS_READWRITE = 0,
+		ACCESS_READONLY  = 1
+	} AccessMode;
+
+	/**
+	 * Object metadata, each object has a meta object that holds its metadata. The metadata define
+	 * properties for each object and can be used by multiple modules (e.g. telemetry and logger)
+	 *
+	 * The object metadata flags are packed into a single 16 bit integer.
+	 * The bits in the flag field are defined as:
+	 *
+	 *   Bit(s)  Name                       Meaning
+	 *   ------  ----                       -------
+	 *      0    access                     Defines the access level for the local transactions (readonly=0 and readwrite=1)
+	 *      1    gcsAccess                  Defines the access level for the local GCS transactions (readonly=0 and readwrite=1), not used in the flight s/w
+	 *      2    telemetryAcked             Defines if an ack is required for the transactions of this object (1:acked, 0:not acked)
+	 *      3    gcsTelemetryAcked          Defines if an ack is required for the transactions of this object (1:acked, 0:not acked)
+	 *    4-5    telemetryUpdateMode        Update mode used by the telemetry module (UAVObjUpdateMode)
+	 *    6-7    gcsTelemetryUpdateMode     Update mode used by the GCS (UAVObjUpdateMode)
+	 */
+	typedef struct {
+		uint16_t flags; /** Defines flags for update and logging modes and whether an update should be ACK'd (bits defined above) */
+		uint16_t flightTelemetryUpdatePeriod; /** Update period used by the telemetry module (only if telemetry mode is PERIODIC) */
+		uint16_t gcsTelemetryUpdatePeriod; /** Update period used by the GCS (only if telemetry mode is PERIODIC) */
+		uint16_t loggingUpdatePeriod; /** Update period used by the logging module (only if logging mode is PERIODIC) */
+	} Metadata;
+
+
+	UAVObject(uint32_t objID, bool isSingleInstance, const std::string &name);
+	void initialize(uint32_t instID);
+	uint32_t getObjID();
+	uint32_t getInstID();
+	bool isSingleInstance();
+	std::string getName();
+	size_t getNumBytes();
+	virtual ssize_t serialize(uint8_t *dataOut);
+	virtual ssize_t deserialize(const uint8_t *dataIn);
+	// bool save();
+	// bool load();
+	virtual void setMetadata(const Metadata &mdata) = 0;
+	virtual Metadata getMetadata();
+	virtual Metadata getDefaultMetadata();
+	void lock();
+	bool lock(int timeoutMs);
+	void unlock();
+	boost::recursive_timed_mutex *getMutex();
+
+	std::string toString();
+	virtual std::toStringData();
+
+	// Metadata accessors
+	static void MetadataInitialize(Metadata &meta);
+	static AccessMode GetFlightAccess(const Metadata &meta);
+	static void SetFlightAccess(Metadata &meta, AccessMode mode);
+	static AccessMode GetGcsAccess(const Metadata &meta);
+	static void SetGcsAccess(Metadata &meta, AccessMode mode);
+	static bool GetFlightTelemetryAcked(const Metadata &meta);
+	static void SetFlightTelemetryAcked(Metadata &meta, bool val);
+	static bool GetGcsTelemetryAcked(const Metadata &meta);
+	static void SetGcsTelemetryAcked(Metadata &meta, bool val);
+	static UpdateMode GetFlightTelemetryUpdateMode(const Metadata &meta);
+	static void SetFlightTelemetryUpdateMode(Metadata &meta, UpdateMode val);
+	static UpdateMode GetGcsTelemetryUpdateMode(const Metadata &meta);
+	static void SetGcsTelemetryUpdateMode(Metadata &meta, UpdateMode val);
+
+	// slots
+	void requestUpdate();
+	void updated();
+
+	boost::signals2::signal<void(UAVObject *)> objectUpdated;
+	boost::signals2::signal<void(UAVObject *)> objectUpdatedAuto;
+	boost::signals2::signal<void(UAVObject *)> objectUpdatedManual;
+	boost::signals2::signal<void(UAVObject *)> objectUpdatedPeriodic;
+	boost::signals2::signal<void(UAVObject *)> updateRequested;
+	boost::signals2::signal<void(UAVObject *)> transactionCompleted;
+	boost::signals2::signal<void(UAVObject *)> newInstance;
+
+protected:
+	uint32_t objID;
+	uint32_t instID;
+	bool isSingleInst;
+	std::string name;
+	boost::recursive_timed_mutex mutex;
+	uint8_t *data;
+	size_t numBytes;
+};
+
+} // namespace openpilot
+
+#endif // UAVOBJECT_H
