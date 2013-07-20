e8a45ce79d146c6db0f883aa0d1e09c97987753f
Vladimir Ermakov <vooon341@gmail.com>
2013-07-20 05:38:52 +0400

    Build: add USE_UAVOBJGEN option for overwrite uavobjgenerator location

--- genuavobj.cmake
+++ genuavobj.cmake
@@ -1,14 +1,26 @@
 # generate UAVOvjects from xml
 
-set(OPENPILOT_DIR "$ENV{HOME}/src/OpenPilot" CACHE STRING "OpenPilot source tree path")
+set(OPENPILOT_DIR "$ENV{HOME}/src/UAV/OpenPilot" CACHE STRING "OpenPilot source tree path")
+set(USE_UAVOBJGEN "" CACHE STRING "Overwrite uavobjgenerator path (discard compile)")
 set(UAVOBJ_BIN "${OPENPILOT_DIR}/build/uavobjgenerator/uavobjgenerator")
 set(UAVOBJ_XML_DIR "${OPENPILOT_DIR}/shared/uavobjectdefinition")
 set(UAVOBJ_TEMPLATE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+set(UAVOBJ_DEPEND "uavobjgenerator")
 
 
 get_filename_component(OPENPILOT_DIR ${OPENPILOT_DIR} REALPATH)
 if(NOT EXISTS "${OPENPILOT_DIR}/Makefile")
 	message(FATAL_ERROR "You must specify OPENPILOT_DIR")
+endif()
+
+if(USE_UAVOBJGEN)
+	get_filename_component(USE_UAVOBJGEN ${USE_UAVOBJGEN} REALPATH)
+	if (NOT EXISTS ${USE_UAVOBJGEN})
+		message(FATAL_ERROR "File not found " ${USE_UAVOBJGEN})
+	else()
+		set(UAVOBJ_BIN ${USE_UAVOBJGEN})
+		set(UAVOBJ_DEPEND "")
+	endif()
 endif()
 
 set(UAVOBJ_SYNTETICS_DIR "rosuavobject")
@@ -25,17 +37,15 @@ list(APPEND UAVOBJ_SYNTETICS_SOURCES "${UAVOBJ_SYNTETICS_DIR}/uavobjectsinit.cpp
 #	message(${f})
 #endforeach(f)
 
-add_custom_command(#TARGET uavobj_syntetics_gen
+add_custom_command(
 	OUTPUT  ${UAVOBJ_SYNTETICS_SOURCES}
 	COMMAND ${UAVOBJ_BIN} -rosgw ${UAVOBJ_XML_DIR} ${UAVOBJ_TEMPLATE_DIR}
 	COMMENT "Generating UAVObjects"
-	DEPENDS uavobjgenerator
+	DEPENDS ${UAVOBJ_DEPEND}
 	)
-#add_dependencies(uavobjects uavobj_syntetics_gen)
 
 add_custom_target(uavobjgenerator
 	COMMAND make -C ${OPENPILOT_DIR} QMAKE=qmake-qt4 uavobjgenerator
 	COMMENT "Compiling UAVObject generator"
 	)
-#add_dependencies(uavobj_syntetics_gen uavobjgenerator)
 
