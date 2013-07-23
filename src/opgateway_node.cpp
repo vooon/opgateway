80d8afe0b2ff326a569b6f3d65bbbfeccf2574fb
Vladimir Ermakov <vooon341@gmail.com>
2013-07-23 05:30:26 +0400

    Node: add conn slots

--- src/opgateway_node.cpp
+++ src/opgateway_node.cpp
@@ -16,8 +16,20 @@
 using namespace openpilot;
 
 
-UAVObjectManager *g_objMngr = NULL;
+boost::shared_ptr<UAVObjectManager> g_objMngr;
+static boost::shared_ptr<TelemetryManager> m_telMngr;
+static boost::shared_ptr<UAVTalkRelay> m_relay;
 
+
+static void telem_connected(void)
+{
+	ROS_INFO("Telemetry connected");
+}
+
+static void telem_disconnected(void)
+{
+	ROS_INFO("Telemetry disconnected");
+}
 
 int main(int argc, char **argv)
 {
@@ -27,28 +39,31 @@ int main(int argc, char **argv)
 
 	std::string serial_port;
 	int serial_baudrate;
-	std::string server_bind;
-	int server_port;
+	std::string relay_bind;
+	int relay_port;
 
 	priv_nh.param<std::string>("serial_port", serial_port, "/dev/ttyUSB0");
 	priv_nh.param<int>("serial_baudrate", serial_baudrate, 57600);
-	priv_nh.param<std::string>("relay_bind", server_bind, "any TODO");
-	priv_nh.param<int>("relay_port", server_port, 9000);
+	priv_nh.param<std::string>("relay_bind", relay_bind, "0.0.0.0");
+	priv_nh.param<int>("relay_port", relay_port, 9000);
 
 	// Initialize UAVObject storage
-	g_objMngr = new UAVObjectManager();
-	UAVObjectsInitialize(g_objMngr);
+	g_objMngr.reset(new UAVObjectManager());
+	UAVObjectsInitialize(g_objMngr.get());
 
 	// Initialize IO devices
 	UAVTalkSerialIO *serial_io = new UAVTalkSerialIO(serial_port, serial_baudrate);
-	UAVTalkUDPIO *relay_io = new UAVTalkUDPIO(server_bind, server_port);
+	UAVTalkUDPIO *relay_io = new UAVTalkUDPIO(relay_bind, relay_port);
 
 	// Start device IO
-	TelemetryManager *telMngr = new TelemetryManager(g_objMngr);
-	telMngr->start(serial_io);
+	m_telMngr.reset(new TelemetryManager(g_objMngr.get()));
+	m_telMngr->connected.connect(telem_connected);
+	m_telMngr->disconnected.connect(telem_disconnected);
+	m_telMngr->start(serial_io);
 
 	// Relay server
-	UAVTalkRelay *relay = new UAVTalkRelay(relay_io, g_objMngr);
+	ROS_INFO_STREAM("UAVTalk Relay listen on " << relay_bind << " port " << relay_port);
+	m_relay.reset(new UAVTalkRelay(relay_io, g_objMngr.get()));
 
 	while (ros::ok()) {
 		ros::spin();
@@ -56,3 +71,4 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
